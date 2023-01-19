31b8a6b9c59cd207307697a57ab0172ddf2cca33
Gopinath E <Gopinath E@LAPTOP-QME61A2J>
2023-01-19 17:21:12 +0530

    githubscript1

---
+++ src/test/java/Practice_Maven/Class_Test1.java
@@ -0,0 +1,13 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class Class_Test1 {
+	@Test
+	public void script1test()
+	{
+		System.out.println("--Githubscript---1");
+	}
+	
+
+}
